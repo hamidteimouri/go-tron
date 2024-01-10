97d1ecae822b06bf2ae2913e95d2d0d440faaba9
Hamid Teimouri <iamhamidteimouri@gmail.com>
2024-01-10 15:43:31 +0330

    Two method added to convert hex and base58 to each other

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hamidteimouri/gotron
 
 go 1.20
+
+require github.com/mr-tron/base58 v1.2.0
--- pkg.go
+++ pkg.go
@@ -1,1 +1,42 @@
 package gotron
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"github.com/mr-tron/base58"
+)
+
+func S256(s []byte) []byte {
+	h := sha256.New()
+	h.Write(s)
+	bs := h.Sum(nil)
+	return bs
+}
+func HexToBase58(str string) (string, error) {
+	addb, err := hex.DecodeString(str)
+	if err != nil {
+		return "", nil
+	}
+	hash1 := S256(S256(addb))
+	secret := hash1[:4]
+	for _, v := range secret {
+		addb = append(addb, v)
+	}
+	return base58.Encode(addb), nil
+}
+
+func Base58ToHex(str string) (string, error) {
+	// Decode base58 string
+	decoded, err := base58.Decode(str)
+	if err != nil {
+		return "", err
+	}
+
+	// Remove the last 4 bytes (checksum) from the decoded data
+	address := decoded[:len(decoded)-4]
+
+	// Convert the remaining data to hexadecimal
+	hexStr := hex.EncodeToString(address)
+
+	return hexStr, nil
+}
